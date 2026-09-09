fc33db0139ebc0f317881da63f5d058e8842086d
Tal <tal.marsh2007@gmail.com>
2026-09-09 16:20:46 +0200

    commit 2

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use crossterm::{cursor, execute, terminal, event::{read, Event, KeyCode}};
-use std::{io::{self, Write}};
+use std::{io::{self, Write}, vec};
 
 fn main() -> io::Result<()> {
 
     terminal::enable_raw_mode()?;
+
+    let mut lines: Vec<String> = vec![String::new()];
 
     // clear and move cursor to 0,0
     execute!(
@@ -18,8 +20,9 @@ fn main() -> io::Result<()> {
         write!(io::stdout(), "~\r\n")?;
     }
     execute!(io::stdout(), cursor::MoveTo(0, 0))?;
-    
-    let mut text =  "".to_string();
+
+    let mut cursor_row = 0;
+    let mut cursor_col = 0;
     let mut mode = 0;
 
     loop {
@@ -36,31 +39,86 @@ fn main() -> io::Result<()> {
                                 )?;
                                 break;
                             }
-                            'j' => execute!(io::stdout(), cursor::MoveDown(1))?,
-                            'k' => execute!(io::stdout(), cursor::MoveUp(1))?,
-                            'h' => execute!(io::stdout(), cursor::MoveLeft(1))?,
-                            'l' => execute!(io::stdout(), cursor::MoveRight(1))?,
+                            'j' => {
+                                if cursor_row < lines.len()-1 {
+                                    cursor_row += 1;
+                                    cursor_col = cursor_col.min(lines[cursor_row].len());
+                                }
+                            }
+                            'k' => {
+                                if cursor_row > 0 {
+                                    cursor_row -= 1;
+                                    cursor_col = cursor_col.min(lines[cursor_row].len());
+                                }
+                            }
+                            'h' => {
+                                if cursor_col > 0 {
+                                    cursor_col -= 1;
+                                }
+                            }
+                            'l' => {
+                                if cursor_col < lines[cursor_row].len() {
+                                    cursor_col += 1;
+                                }
+                            }
                             _ => {}
                         }
                     } else if mode == 1 {
-                        text.push(c);
-                        write!(io::stdout(), "{c}")?;
-                        execute!(io::stdout())?;
+                        lines[cursor_row].insert(cursor_col, c);
+                        // redraw whole line
+                        execute!(io::stdout(), cursor::MoveTo(0, cursor_row as u16))?;
+                        write!(io::stdout(), "{}", lines[cursor_row])?;
+                        io::stdout().flush()?;
+                        cursor_col += 1;
                     }
                 }
                 KeyCode::Esc => {
                     mode = 1 - mode
                 }
-                KeyCode::Enter => write!(io::stdout(), "\r\n")?,
+                KeyCode::Enter => {
+                    // basically need to do the opposite of backspace :/
+                    write!(io::stdout(), "\r\n")?;
+                    cursor_row += 1;
+                    cursor_col = 0;
+                    lines.insert(cursor_row, String::new());
+                }
                 KeyCode::Backspace => {
+                    //handle on line or back a line
                     if mode == 1 {
-                        execute!(io::stdout(), cursor::MoveLeft(1))?;
-                        write!(io::stdout(), " ")?;
-                        execute!(io::stdout(), cursor::MoveLeft(1))?;
+                        if cursor_col > 0 {
+                            // cursor should delete character behind it
+                            lines[cursor_row].remove(cursor_col-1);
+                            cursor_col -= 1;
+                            execute!(io::stdout(),
+                                    cursor::MoveTo(0, cursor_row as u16),
+                                    terminal::Clear(terminal::ClearType::CurrentLine)
+                                    )?;
+                            write!(io::stdout(), "{}", lines[cursor_row])?;
+                            io::stdout().flush()?;
+                        } else {
+                            if cursor_row > 0 {
+                                let current_line = lines.remove(cursor_row);
+                                cursor_col = lines[cursor_row - 1].len();
+                                lines[cursor_row - 1].push_str(&current_line);
+                                cursor_row -= 1;
+                                // print entire text again ig
+                                for i in 0..(rows as usize) {
+                                    execute!(io::stdout(),
+                                            cursor::MoveTo(0, i as u16),
+                                            terminal::Clear(terminal::ClearType::CurrentLine))?;
+                                    if i < lines.len() {
+                                        write!(io::stdout(), "{}", lines[i])?;
+                                    } else {
+                                        write!(io::stdout(), "~")?;
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
                 _ => {}
             }
+            execute!(io::stdout(), cursor::MoveTo(cursor_col as u16, cursor_row as u16))?;
         }
     }
     
